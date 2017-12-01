8f23fbf50c8eb53b0e06cba699e122d5e2d83cf8
Hiep Do <gao619@gmail.com>
2017-12-01 10:39:41 -0500

    add homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $ :any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +10,26 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var carousel = $(".carousel"),
+    currdeg  = 0;
+
+$(".next").on("click", { d: "n" }, rotate);
+$(".prev").on("click", { d: "p" }, rotate);
+
+function rotate(e){
+  if(e.data.d=="n"){
+    currdeg = currdeg - 60;
+  }
+  if(e.data.d=="p"){
+    currdeg = currdeg + 60;
+  }
+  carousel.css({
+    "-webkit-transform": "rotateY("+currdeg+"deg)",
+    "-moz-transform": "rotateY("+currdeg+"deg)",
+    "-o-transform": "rotateY("+currdeg+"deg)",
+    "transform": "rotateY("+currdeg+"deg)"
+  });
+}
   }
 
 }
